c35df024d1b523b461c0c674ee46c9049f90d6c5
marco <marcorampok@outlook.com>
2017-01-03 10:38:07 +0800

    bundle fix

--- AnalogTest/AppDelegate.swift
+++ AnalogTest/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AnalogBridgeController
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        AnalogBridgeRunner.sharedRunner.setDefaultPublicKey(key: "pk_test_p7497xd1I92AHJBx4OLzjuC5")
+        AnalogBridgeRunner.sharedRunner.run(window: window!)
+        
         return true
     }
 
