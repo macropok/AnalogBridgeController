51c9ec7c1da6e726ae16a4c9fe67c851336c5509
marco <marcorampok@outlook.com>
2017-01-03 11:51:59 +0800

    png bundle fix

--- AnalogBridgeController/SlideMenuController/UIViewController.swift
+++ AnalogBridgeController/SlideMenuController/UIViewController.swift
@@ -10,22 +10,24 @@ import UIKit
 
 extension UIViewController {
     
-    
-    
     func setNavigationBarItem() {
-        self.addLeftBarButtonWithImage(UIImage(named: "ic_menu_black_24dp")!)
+        
+        let podBundle = Bundle(for: self.classForCoder)
+        let bundleURL = podBundle.url(forResource: "AnalogBridgeController", withExtension: "bundle", subdirectory: nil)
+        
+        self.addLeftBarButtonWithImage(UIImage(named: "ic_menu_black_24dp", in: Bundle(url: bundleURL!), compatibleWith: nil)!)
         self.slideMenuController()?.removeLeftGestures()
         self.slideMenuController()?.removeRightGestures()
         self.slideMenuController()?.addLeftGestures()
         
         
-        let orderHistoryButton:UIBarButtonItem = UIBarButtonItem(image: UIImage(named: "ic_person"), style: .plain, target: self, action: #selector(self.toggleOrderHistory))
+        let orderHistoryButton:UIBarButtonItem = UIBarButtonItem(image: UIImage(named: "ic_person", in: Bundle(url: bundleURL!), compatibleWith: nil)!, style: .plain, target: self, action: #selector(self.toggleOrderHistory))
         
         var shoppingCartButton:MJBadgeBarButton!
         let customButton = UIButton(type: UIButtonType.custom)
         customButton.frame = CGRect(x: 0, y: 0, width: 35.0, height: 35.0)
         customButton.addTarget(self, action: #selector(self.toggleShoppingCart), for: .touchUpInside)
-        customButton.setImage(UIImage(named: "ic_remove_shopping_cart"), for: .normal)
+        customButton.setImage(UIImage(named: "ic_remove_shopping_cart", in: Bundle(url: bundleURL!), compatibleWith: nil)!, for: .normal)
         
         shoppingCartButton = MJBadgeBarButton()
         shoppingCartButton.setup(customButton: customButton)
@@ -34,7 +36,7 @@ extension UIViewController {
         shoppingCartButton.badgeOriginX = 20.0
         shoppingCartButton.badgeOriginY = -4
         
-        let exitButton:UIBarButtonItem = UIBarButtonItem(image: UIImage(named: "ic_exit_to_app"), style: .plain, target: self, action: #selector(self.toggleExitApp))
+        let exitButton:UIBarButtonItem = UIBarButtonItem(image: UIImage(named: "ic_exit_to_app", in: Bundle(url: bundleURL!), compatibleWith: nil)!, style: .plain, target: self, action: #selector(self.toggleExitApp))
         
         self.navigationItem.rightBarButtonItems = [exitButton, shoppingCartButton, orderHistoryButton]
         for button in self.navigationItem.rightBarButtonItems! {
@@ -57,15 +59,21 @@ extension UIViewController {
     }
     
     func toggleOrderHistory() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let podBundle = Bundle(for: self.classForCoder)
+        let bundleURL = podBundle.url(forResource: "AnalogBridgeController", withExtension: "bundle", subdirectory: nil)
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle(url:bundleURL!))
+        
         let orderHistoryController = storyboard.instantiateViewController(withIdentifier: "orderHistoryController")
         let navController = UINavigationController(rootViewController: orderHistoryController)
         self.slideMenuController()?.changeMainViewController(navController, close: true)
     }
     
     func toggleShoppingCart() {
-        print ("shopping toggle clicked")
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let podBundle = Bundle(for: self.classForCoder)
+        let bundleURL = podBundle.url(forResource: "AnalogBridgeController", withExtension: "bundle", subdirectory: nil)
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle(url:bundleURL!))
         let shoppingCartController = storyboard.instantiateViewController(withIdentifier: "shoppingCartController")
         let navController = UINavigationController(rootViewController: shoppingCartController)
         self.slideMenuController()?.changeMainViewController(navController, close: true)
