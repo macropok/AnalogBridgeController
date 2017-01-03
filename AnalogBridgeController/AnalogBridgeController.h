c924c1ab3e251fe417f0a6595dbb3589a9d679e6
marco <marcorampok@outlook.com>
2017-01-03 09:31:03 +0800

    Initial Commit

---
+++ AnalogBridgeController/AnalogBridgeController.h
@@ -0,0 +1,19 @@
+//
+//  AnalogBridgeController.h
+//  AnalogBridgeController
+//
+//  Created by PSIHPOK on 1/3/17.
+//  Copyright © 2017 marco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnalogBridgeController.
+FOUNDATION_EXPORT double AnalogBridgeControllerVersionNumber;
+
+//! Project version string for AnalogBridgeController.
+FOUNDATION_EXPORT const unsigned char AnalogBridgeControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalogBridgeController/PublicHeader.h>
+
+
